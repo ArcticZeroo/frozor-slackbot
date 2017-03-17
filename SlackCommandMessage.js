17c331512e287d44d4228d352f3595d48acaf8c1
Spencer <spencer@artix.cc>
2017-03-16 23:42:06 -0400

    Fix spaces

--- SlackCommandMessage.js
+++ SlackCommandMessage.js
@@ -4,7 +4,7 @@ class SlackCommandMessage extends SlackMessage{
     constructor(msg, bot){
         super(msg, bot);
 
-        this.args = this.text.split(' ').slice(1);
+        this.args = this.text.split(' ').slice(1).filter((arg)=> arg.trim() != '');
         this.commandName = this.args[0];
         this.args.shift();
     }
