6190d264dbe57fd44ec8fbfb00bade287c8b5121
Spencer <spencer@artix.cc>
2017-04-23 21:53:49 -0400

    Export SlackCommandMessage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     SlackBot: require('./lib/SlackBot'),
     SlackMessage: require('./lib/SlackMessage'),
+    SlackCommandMessage: require('./lib/SlackCommandMessage'),
     SlackUser: require('./lib/SlackUser'),
     ChatHandler: require('./lib/ChatHandler')
 };
